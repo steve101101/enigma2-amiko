25c9519029fd4ca90d874043ad3545ce00f595cf
weter71 <i.lit@gmx.de>
2011-11-25 01:17:27 +0100

    fix

--- enigma2/bin/setspark.sh
+++ enigma2/bin/setspark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh              
 fw_setenv bootargs "console=ttyAS0,115200 rw ramdisk_size=6144 init=/linuxrc root=/dev/ram0 nwhwconf=device:eth0,hwaddr:00:80:E1:12:40:69 ip=${ipaddr}:${serverip}:${gatewayip}:${netmask}:lh:eth0:off stmmaceth=msglvl:0,phyaddr:1,watchdog:5000 bigphysarea=4000"
 fw_setenv bootcmd "bootm  0x00080000"
 fw_setenv boot_system "spark"
