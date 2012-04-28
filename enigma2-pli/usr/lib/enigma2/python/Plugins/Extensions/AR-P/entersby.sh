3eb58aa816792ce20dfaf792a0732fa3040201a4
schpuntik <schpuntik@freenet.de>
2012-04-28 11:55:51 +0200

    change  *.pyc to *.pyo

--- enigma2-pli/usr/lib/enigma2/python/Plugins/Extensions/AR-P/entersby.sh
+++ enigma2-pli/usr/lib/enigma2/python/Plugins/Extensions/AR-P/entersby.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-if [ ! `grep -o spark /proc/stb/info/model` ]; then
- /bin/vdstandby &
-else
-# turn off TV
-/bin/stfbcontrol hd
-
- echo off > /dev/vfd
-sleep 2
-date +%H%M > /dev/vfd
-
-fi
+/bin/fp_control -s `date +"%H:%M:%S %d-%m-%Y"`
--- enigma2/usr/lib/enigma2/python/Plugins/Extensions/AR-P/entersby.sh
+++ enigma2/usr/lib/enigma2/python/Plugins/Extensions/AR-P/entersby.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-if [ ! `grep -o spark /proc/stb/info/model` ]; then
- /bin/vdstandby &
+/bin/stfbcontrol hd 
+echo off > /dev/vfd 
+sleep 2 
+DISPL7162=`grep -c 'spark7162' /proc/stb/info/model`
+if [ "$DISPL7162" -eq "1" ]; then
+/bin/fp_control -c
+/bin/fp_control -s `date +"%H:%M:%S %d-%m-%Y"`
 else
-# turn off TV
-/bin/stfbcontrol hd
-
- echo off > /dev/vfd
-sleep 2
-date +%H%M > /dev/vfd
-
+/bin/fp_control -s `date +"%H:%M:%S %d-%m-%Y"`
 fi
+echo "done"
